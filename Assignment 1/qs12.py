0d1986274417b068b3b45eede78c6c11b584f578
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-03-03 00:01:56 +0530

    qs 12 done

--- Assignment 1/qs12.py
+++ Assignment 1/qs12.py
@@ -3,8 +3,9 @@ with open ("first.txt","w") as file:
     file.writelines("Carry umbrella and overcoat when it rains.")
 
 with open ("first.txt","r") as file:  
-    r = file.readlines()
+    r = file.read().split() # it will break the line from white spaces and help to seperate each words
+    print(r)
     for word in r:
         if word[0] in "aeiou":
-            file1.write(word)
+            file1.write(word+"\n")
     
