9186bf6b4c8ddfdabbeea03cebfcd35bf8768ca9
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-03-03 23:41:37 +0530

    done

--- Assignment 1/qs20.py
+++ Assignment 1/qs20.py
@@ -1,3 +1,19 @@
 with open ("f1.txt","r")as file:
-    r = file.read().strip().split()
-    
+    r = file.readlines()
+    modified_lines = []
+
+    for line in r:
+        words = line.strip().split()
+        filtered_words=[] 
+
+        for i in range(len(words)):
+            if i%2 == 0:
+                filtered_words.append(words[i])
+        
+        modified_lines.append(" ".join(filtered_words))
+
+print(modified_lines)
+
+with open("f1.txt","a")as file:
+    for lines in modified_lines:
+        file.write(lines+"\n")
