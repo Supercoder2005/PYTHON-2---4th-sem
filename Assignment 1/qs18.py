8ce64e0ef9daf0575cd04c9ff37a7b37e61a7578
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-03-03 22:14:50 +0530

    qs 18 done

--- Assignment 1/qs18.py
+++ Assignment 1/qs18.py
@@ -1,8 +1,9 @@
+with open ("numbers.txt","w")as file:
+    file.write("The price of 3 apples is 150 rupees and 2 mangoes cost 80")
 with open ("numbers.txt","r")as file:
     r = file.read().strip().split()
     num = []
     for words in r:
         if words.isdigit():
-            print(words)
             num.append(words)
     print(num)
