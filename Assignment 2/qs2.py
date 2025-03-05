f9b3db9b27335be7122d4a42dd760240cb5b8f75
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-03-06 01:58:25 +0530

    qs 2 done

---
+++ Assignment 2/qs2.py
@@ -0,0 +1,14 @@
+import csv 
+with open("details.csv","r")as file:
+    cread = csv.reader(file)
+    list = list(cread)
+    for i in range(1,len(list)):
+        print(list[i]) 
+
+import csv 
+with open("details.csv","r")as file:
+    cread = csv.reader(file)
+    next(cread) #skips the first line
+    for rows in cread:
+        print(rows)
+    
